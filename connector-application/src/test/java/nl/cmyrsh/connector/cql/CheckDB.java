d3881c0ce1f1784bb6cced0cf61dda316c759ed4
Mayuresh <myrsh.nl@gmail.com>
2021-01-03 23:07:12 +0100

    fixed resultset

--- connector-application/src/test/java/nl/cmyrsh/connector/cql/CheckDB.java
+++ connector-application/src/test/java/nl/cmyrsh/connector/cql/CheckDB.java
@@ -1,16 +1,26 @@
 package nl.cmyrsh.connector.cql;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Spliterator;
+import java.util.Spliterators;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
+import com.datastax.oss.driver.api.core.cql.ColumnDefinition;
 import com.datastax.oss.driver.api.core.cql.PreparedStatement;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+import com.datastax.oss.driver.api.core.type.codec.registry.MutableCodecRegistry;
+import com.datastax.oss.driver.api.core.type.reflect.GenericType;
 import com.datastax.oss.quarkus.runtime.api.session.QuarkusCqlSession;
 
 import io.quarkus.runtime.StartupEvent;
+import nl.cmyrsh.connector.util.codec.Avro2CassandraTEXTCodec;
 
 @ApplicationScoped
 public class CheckDB {
@@ -27,19 +37,48 @@ public class CheckDB {
     private PreparedStatement getTransactionProgress;
 
     void prepareStatements(@Observes StartupEvent onStart) {
+        ((MutableCodecRegistry) session.getContext().getCodecRegistry()).register(new Avro2CassandraTEXTCodec());
         getTrasaction = session.prepare(GET_FROM_TRANSACTION);
         getTransactionPayload = session.prepare(GET_FROM_TRANSACTION_PAYLOAD);
         getTransactionProgress = session.prepare(GET_FROM_TRANSACTION_PROGRESS);
     }
 
     public String getReport(String transactionId) {
-        String getTx = session.execute(getTrasaction.bind(transactionId)).all().stream().map(row -> row.toString())
-                .collect(Collectors.joining("\n"));
 
-        String getTxPayload = session.execute(getTransactionPayload.bind(transactionId)).all().stream()
-                .map(row -> row.toString()).collect(Collectors.joining("\n"));
-        String getTxProgress = session.execute(getTransactionProgress.bind(transactionId)).all().stream()
-                .map(row -> row.toString()).collect(Collectors.joining("\n"));
+
+        ResultSet transactions = session.execute(getTrasaction.bind(transactionId));
+        ResultSet transactionPayloads = session.execute(getTransactionPayload.bind(transactionId));
+        ResultSet transactionProgress = session.execute(getTransactionProgress.bind(transactionId));
+
+        List<ColumnDefinition> txColums = StreamSupport
+            .stream(transactions.getColumnDefinitions().spliterator(), false)
+            .collect(Collectors.toList());
+        List<ColumnDefinition> txPldColums = StreamSupport
+            .stream(transactionPayloads.getColumnDefinitions().spliterator(), false)
+            .collect(Collectors.toList());
+        List<ColumnDefinition> txPrgColums = StreamSupport
+            .stream(transactionProgress.getColumnDefinitions().spliterator(), false)
+            .collect(Collectors.toList());
+
+        String getTx = StreamSupport.stream(
+                            Spliterators.spliteratorUnknownSize(
+                                transactions.iterator(), Spliterator.ORDERED), 
+                                false)
+                                .map(r -> extract(r, txColums))
+                                .collect(Collectors.joining("\n"));
+
+        String getTxPayload = StreamSupport.stream(
+                            Spliterators.spliteratorUnknownSize(
+                                transactionPayloads.iterator(), Spliterator.ORDERED), 
+                                false)
+                                .map(r -> extract(r, txPldColums))
+                                .collect(Collectors.joining("\n"));
+        String getTxProgress = StreamSupport.stream(
+                            Spliterators.spliteratorUnknownSize(
+                                transactionProgress.iterator(), Spliterator.ORDERED), 
+                                false)
+                                .map(r -> extract(r, txPrgColums))
+                                .collect(Collectors.joining("\n"));
 
         return new StringBuilder().append("=================================================").append("\n")
                 .append("----------------------TransactionData-------------------").append(getTx).append("\n")
@@ -48,5 +87,13 @@ public class CheckDB {
                 .append(getTxProgress).append("\n").append("=================================================")
                 .toString();
 
+    }
+
+
+    private String extract(Row row, List<ColumnDefinition> columns) {
+        return columns.stream()
+            .map(c -> row.getObject(c.getName()))
+            .map(rv -> rv.toString())
+            .collect(Collectors.joining(","));
     }
 }
